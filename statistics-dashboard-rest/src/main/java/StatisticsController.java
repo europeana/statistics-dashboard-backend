549c49ef03d040975222d65970a73236b4c8ff19
Joana Sousa <joana.sousa@europeana.eu>
2021-08-06 17:30:09 +0200

    MET-3764: Created Statistics Controller

---
+++ statistics-dashboard-rest/src/main/java/StatisticsController.java
@@ -0,0 +1,15 @@
+
+public class StatisticsController {
+
+  public static final String GENERAL_STATISTICS = "/statistics/europeana/general";
+  public static final String FILTERING_STATISTICS = "statistics/filtering";
+
+  public void getGeneralStatistics(){
+
+  }
+
+  public void getFilters(){
+
+  }
+
+}
