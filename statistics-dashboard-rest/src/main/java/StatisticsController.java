1af758de03e10c93423830a874d82e7bd4d9c2c2
Joana Sousa <joana.sousa@europeana.eu>
2021-08-10 14:49:12 +0200

    MET-3764: Added new dependencies for spring mvc
    Added new annotations for requests

--- statistics-dashboard-rest/src/main/java/StatisticsController.java
+++ statistics-dashboard-rest/src/main/java/StatisticsController.java
@@ -1,14 +1,28 @@
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class StatisticsController {
 
   public static final String GENERAL_STATISTICS = "/statistics/europeana/general";
   public static final String FILTERING_STATISTICS = "statistics/filtering";
+  public static final String APPLICATION_JSON = "application/json";
 
+  @GetMapping(value = GENERAL_STATISTICS, consumes = {APPLICATION_JSON},
+      produces = {APPLICATION_JSON})
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
   public void getGeneralStatistics(){
 
   }
 
-  public void getFilters(){
+  @PostMapping(value = FILTERING_STATISTICS, consumes = {APPLICATION_JSON},
+      produces = {APPLICATION_JSON})
+  @ResponseStatus(HttpStatus.OK)
+  public void getFilters(@RequestBody String filters){
 
   }
 
