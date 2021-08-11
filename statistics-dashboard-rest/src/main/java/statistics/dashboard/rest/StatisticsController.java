5d7d31b3444e099a434691fb84440879fb197f66
Joana Sousa <joana.sousa@europeana.eu>
2021-08-11 17:11:47 +0200

    MET-3764: Updated pom files and StatisticsController

--- statistics-dashboard-rest/src/main/java/statistics/dashboard/rest/StatisticsController.java
+++ statistics-dashboard-rest/src/main/java/statistics/dashboard/rest/StatisticsController.java
@@ -1,13 +1,16 @@
 package statistics.dashboard.rest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import statistics.dashboard.common.view.FilteringResult;
 import statistics.dashboard.common.view.ResultListFilters;
 
+@Controller
 public class StatisticsController {
 
   public static final String GENERAL_STATISTICS = "/statistics/europeana/general";
@@ -25,8 +28,8 @@ public class StatisticsController {
   @PostMapping(value = FILTERING_STATISTICS, consumes = {APPLICATION_JSON},
       produces = {APPLICATION_JSON})
   @ResponseStatus(HttpStatus.OK)
-  public void getFilters(@RequestBody String filters){
-
+  public FilteringResult getFilters(@RequestBody String filters){
+    return null;
   }
 
 }
