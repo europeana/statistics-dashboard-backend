ca490652c263768b5ebb2efdc217e658847238a3
Joana Sousa <joana.sousa@europeana.eu>
2021-08-23 11:39:58 +0200

    MET-3763: Created new server class

---
+++ statistics-dashboard-service/src/main/java/eu/europeana/statistics/dashboard/service/server/StatisticsServer.java
@@ -0,0 +1,9 @@
+package eu.europeana.statistics.dashboard.service.server;
+
+/**
+ * This class is responsible for retrieving the statistics data
+ * that is requested
+ */
+public class StatisticsServer {
+
+}
