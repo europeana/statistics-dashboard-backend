0edd11871a15436fcec9f137bd79049d048969bf
Joana Sousa <joana.sousa@europeana.eu>
2021-08-25 17:03:46 +0200

    MET-3763: Started to work on mongo queries

--- statistics-dashboard-service/src/main/java/eu/europeana/statistics/dashboard/service/server/StatisticsServer.java
+++ statistics-dashboard-service/src/main/java/eu/europeana/statistics/dashboard/service/server/StatisticsServer.java
@@ -1,10 +1,17 @@
 package eu.europeana.statistics.dashboard.service.server;
 
 import eu.europeana.statistics.dashboard.common.api.request.StatisticsFilteringRequest;
+import eu.europeana.statistics.dashboard.common.api.response.BreakdownResult;
 import eu.europeana.statistics.dashboard.common.api.response.FilteringResult;
 import eu.europeana.statistics.dashboard.common.api.response.ResultListFilters;
+import eu.europeana.statistics.dashboard.service.persistence.Field;
 import eu.europeana.statistics.dashboard.service.persistence.MongoSDDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import eu.europeana.statistics.dashboard.service.persistence.StatisticsData;
+import eu.europeana.statistics.dashboard.service.persistence.StatisticsQuery;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 /**
@@ -20,18 +27,37 @@ public class StatisticsServer {
    * Constructor for the Statistics Server
    * @param mongoSDDao - The mongo database it connects to
    */
-  @Autowired
   public StatisticsServer(MongoSDDao mongoSDDao){
     this.mongoSDDao = mongoSDDao;
   }
 
   public ResultListFilters queryGeneralEuropeanaData(){
-    System.out.println("Testing spring annotations and  connections");
-    return null;
+    ArrayList<StatisticsData> generalQueries = prepareGeneralQueries();
+
+    List<BreakdownResult> allBreakdowns = new ArrayList<>();
+
+//    for(StatisticsData resultData : resultGeneralQuery.getBreakdown()){
+//
+//    }
+    return new ResultListFilters(allBreakdowns);
   }
 
   public FilteringResult queryDataWithFilters(StatisticsFilteringRequest statisticsFilteringRequest){
     return null;
+  }
+
+  private ArrayList<StatisticsData> prepareGeneralQueries(){
+    StatisticsQuery query = mongoSDDao.createStatisticsQuery();
+    ArrayList<StatisticsData> queries = new ArrayList<>();
+
+    List<Field> filterFields = Arrays.stream(Field.values())
+        .filter(x -> x != Field.UPDATED_DATE && x != Field.CREATED_DATE && x != Field.DATASET_ID)
+        .collect(Collectors.toUnmodifiableList());
+    for(Field field : filterFields) {
+      queries.add(query.withBreakdowns(field).queryForStatistics());
+    }
+
+    return queries;
   }
 
 }
