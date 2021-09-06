c2a6835e0e279a52af7de06e9fcb75df3b16ebec
Joana Sousa <joana.sousa@europeana.eu>
2021-09-06 12:36:51 +0200

    MET-3763: Renamed some FilterNames so it could match with Front-End

--- statistics-dashboard-common/src/main/java/eu/europeana/statistics/dashboard/common/utils/FilterNames.java
+++ statistics-dashboard-common/src/main/java/eu/europeana/statistics/dashboard/common/utils/FilterNames.java
@@ -3,13 +3,15 @@ package eu.europeana.statistics.dashboard.common.utils;
 public final class FilterNames {
 
   // FACET VALUES AS STRINGS
+  //TODO 06-09-2021: At the moment, some strings have different cases so it can work with the front-end.
+  //TODO: Eventually, we should put it back to camelCase so that it's consistent
   public static final String CONTENT_TIER = "contentTier";
-  public static final String CONTENT_TYPE = "contentType";
-  public static final String COUNTRY = "country";
-  public static final String DATA_PROVIDER = "dataProvider";
+  public static final String CONTENT_TYPE = "TYPE";
+  public static final String COUNTRY = "COUNTRY";
+  public static final String DATA_PROVIDER = "DATA_PROVIDER";
   public static final String METADATA_TIER = "metadataTier";
-  public static final String PROVIDER = "provider";
-  public static final String RIGHTS_STATEMENTS = "rightsStatements";
+  public static final String PROVIDER = "PROVIDER";
+  public static final String RIGHTS_STATEMENTS = "RIGHTS";
   public static final String CREATED_DATE = "createdDate";
   public static final String UPDATED_DATE = "updatedDate";
 
