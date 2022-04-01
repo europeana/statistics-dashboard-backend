8b9763d4bf1eb5944a2f5ba0ed3076f26b443f9d
Jorge Ortiz <jorge.ortiz@europeana.eu>
2022-04-01 17:02:49 +0200

    MET-4256 add unit tests

---
+++ statistics-dashboard-rest/src/test/java/eu/europeana/statistics/dashboard/rest/StatisticsControllerTest.java
@@ -0,0 +1,106 @@
+package eu.europeana.statistics.dashboard.rest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import eu.europeana.statistics.dashboard.common.api.request.FiltersWrapper;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsBreakdownValueFilter;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsFilteringRequest;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsRangeFilter;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsValueFilter;
+import eu.europeana.statistics.dashboard.common.api.response.FilteringOptions;
+import eu.europeana.statistics.dashboard.common.api.response.FilteringResult;
+import eu.europeana.statistics.dashboard.common.api.response.ResultListFilters;
+import eu.europeana.statistics.dashboard.common.api.response.StatisticsResult;
+import eu.europeana.statistics.dashboard.service.StatisticsService;
+import eu.europeana.statistics.dashboard.service.exception.BreakdownDeclarationFailException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class StatisticsControllerTest {
+
+  @Mock
+  private StatisticsService statisticsService;
+
+  @InjectMocks
+  private StatisticsController controller;
+
+  @Test
+  void getGeneralStatisticsWithoutContentTierZeroNoParam() {
+    ResultListFilters resultListFilters = new ResultListFilters(List.of());
+    when(statisticsService.queryGeneralEuropeanaDataWithoutContentTierZero()).thenReturn(resultListFilters);
+
+    ResultListFilters testResult = controller.getGeneralStatistics(Optional.empty());
+
+    assertEquals(resultListFilters, testResult);
+  }
+
+  @Test
+  void getGeneralStatisticsWithoutContentTierZero() {
+    ResultListFilters resultListFilters = new ResultListFilters(List.of());
+    when(statisticsService.queryGeneralEuropeanaDataWithoutContentTierZero()).thenReturn(resultListFilters);
+
+    ResultListFilters testResult = controller.getGeneralStatistics(Optional.of(false));
+
+    assertEquals(resultListFilters, testResult);
+  }
+
+  @Test
+  void getGeneralStatisticsIncludingContentTierZero() {
+    ResultListFilters resultListFilters = new ResultListFilters(List.of());
+    when(statisticsService.queryGeneralEuropeanaDataIncludingContentTierZero()).thenReturn(resultListFilters);
+
+    ResultListFilters testResult = controller.getGeneralStatistics(Optional.of(true));
+
+    assertEquals(resultListFilters, testResult);
+  }
+
+  @Test
+  void getFilters() throws BreakdownDeclarationFailException {
+    FiltersWrapper filtersWrapper = new FiltersWrapper();
+    FilteringResult filteringResult = new FilteringResult(new StatisticsResult("value", 5, 100),
+        new FilteringOptions(new HashSet<>() {{
+          add("a");
+        }},
+            new HashSet<>(), new HashSet<>(), new HashSet<>(), new HashSet<>(),
+            new HashSet<>(), new HashSet<>(), null, null));
+    when(statisticsService.queryDataWithFilters(any())).thenReturn(filteringResult);
+
+    FilteringResult testFilteringResult = controller.getFilters(filtersWrapper);
+
+    assertEquals(testFilteringResult, filteringResult);
+  }
+
+  @Test
+  void getFiltersWithException() {
+    FiltersWrapper filtersWrapper = new FiltersWrapper();
+    filtersWrapper.setFilters(new StatisticsFilteringRequest(
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsValueFilter(List.of("1")),
+        new StatisticsRangeFilter("", ""),
+        new StatisticsRangeFilter("", "")
+    ));
+
+    Throwable testResult = assertThrows(BreakdownDeclarationFailException.class, () ->
+    {
+      when(statisticsService.queryDataWithFilters(any())).thenThrow(new BreakdownDeclarationFailException("Error"));
+      controller.getFilters(filtersWrapper);
+    });
+    assertEquals("Error", testResult.getMessage());
+  }
+}
---
+++ statistics-dashboard-service/src/test/java/eu/europeana/statistics/dashboard/service/StatisticsServiceTest.java
@@ -0,0 +1,162 @@
+package eu.europeana.statistics.dashboard.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import eu.europeana.statistics.dashboard.common.api.request.FiltersWrapper;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsBreakdownValueFilter;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsFilteringRequest;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsRangeFilter;
+import eu.europeana.statistics.dashboard.common.api.request.StatisticsValueFilter;
+import eu.europeana.statistics.dashboard.common.api.response.FilteringResult;
+import eu.europeana.statistics.dashboard.common.api.response.ResultListFilters;
+import eu.europeana.statistics.dashboard.common.iternal.MongoStatisticsField;
+import eu.europeana.statistics.dashboard.service.exception.BreakdownDeclarationFailException;
+import eu.europeana.statistics.dashboard.service.persistence.MongoSDDao;
+import eu.europeana.statistics.dashboard.service.persistence.StatisticsData;
+import eu.europeana.statistics.dashboard.service.persistence.StatisticsQuery;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class StatisticsServiceTest {
+
+  @Mock
+  private StatisticsQuery statisticsQuery;
+
+  @Mock
+  private StatisticsQuery statisticsQuery2;
+
+  @Mock
+  private StatisticsQuery statisticsQuery3;
+
+  @Mock
+  private StatisticsData statisticsData;
+
+  @Mock
+  private MongoSDDao mongoSDDao;
+
+  @InjectMocks
+  private StatisticsService statisticsService;
+
+  @Test
+  void queryGeneralEuropeanaDataWithoutContentTierZero() {
+    when(statisticsData.getBreakdown()).thenReturn(List.of(statisticsData));
+    when(statisticsData.getField()).thenReturn(MongoStatisticsField.CONTENT_TIER)
+                                   .thenReturn(MongoStatisticsField.COUNTRY)
+                                   .thenReturn(MongoStatisticsField.DATA_PROVIDER)
+                                   .thenReturn(MongoStatisticsField.METADATA_TIER)
+                                   .thenReturn(MongoStatisticsField.PROVIDER)
+                                   .thenReturn(MongoStatisticsField.RIGHTS)
+                                   .thenReturn(MongoStatisticsField.TYPE);
+    when(statisticsData.getRecordCount()).thenReturn(12);
+    when(statisticsData.getFieldValue())
+        .thenReturn("1")
+        .thenReturn("2")
+        .thenReturn("3")
+        .thenReturn("4");
+
+    when(statisticsQuery3.queryForStatistics()).thenReturn(statisticsData);
+    when(statisticsQuery2.withValueFilter(any(MongoStatisticsField.class), any(List.class))).thenReturn(statisticsQuery3);
+    when(statisticsQuery.withBreakdowns(any(MongoStatisticsField.class))).thenReturn(statisticsQuery2);
+    when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
+
+    ResultListFilters resultListFilters = statisticsService.queryGeneralEuropeanaDataWithoutContentTierZero();
+
+    assertEquals(7, resultListFilters.getAllBreakdowns().size());
+    assertTrue(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rights", "type").equals(
+        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList())));
+  }
+
+  @Test
+  void queryGeneralEuropeanaDataIncludingContentTierZero() {
+    when(statisticsData.getBreakdown()).thenReturn(List.of(statisticsData));
+    when(statisticsData.getField()).thenReturn(MongoStatisticsField.CONTENT_TIER)
+                                   .thenReturn(MongoStatisticsField.COUNTRY)
+                                   .thenReturn(MongoStatisticsField.DATA_PROVIDER)
+                                   .thenReturn(MongoStatisticsField.METADATA_TIER)
+                                   .thenReturn(MongoStatisticsField.PROVIDER)
+                                   .thenReturn(MongoStatisticsField.RIGHTS)
+                                   .thenReturn(MongoStatisticsField.TYPE);
+    when(statisticsData.getRecordCount()).thenReturn(12);
+    when(statisticsData.getFieldValue())
+        .thenReturn("0")
+        .thenReturn("1")
+        .thenReturn("2")
+        .thenReturn("3")
+        .thenReturn("4");
+    when(statisticsQuery2.queryForStatistics()).thenReturn(statisticsData);
+    when(statisticsQuery.withBreakdowns(any(MongoStatisticsField.class))).thenReturn(statisticsQuery2);
+    when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
+
+    ResultListFilters resultListFilters = statisticsService.queryGeneralEuropeanaDataIncludingContentTierZero();
+
+    assertEquals(7, resultListFilters.getAllBreakdowns().size());
+    assertTrue(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rights", "type").equals(
+        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList())));
+  }
+
+  @Test
+  void queryDataWithFilters() throws BreakdownDeclarationFailException {
+    FiltersWrapper filtersWrapper = new FiltersWrapper();
+    filtersWrapper.setFilters(new StatisticsFilteringRequest(
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(1),
+        null,
+        null,
+        null,
+        null,
+        null,
+        new StatisticsValueFilter(List.of("1")),
+        new StatisticsRangeFilter("", ""),
+        new StatisticsRangeFilter("", "")
+    ));
+    when(statisticsData.getBreakdown()).thenReturn(List.of(statisticsData));
+    when(statisticsData.getField()).thenReturn(MongoStatisticsField.CONTENT_TIER)
+                                   .thenReturn(MongoStatisticsField.CONTENT_TIER);
+    when(statisticsData.getRecordCount()).thenReturn(5);
+    when(statisticsData.getFieldValue())
+        .thenReturn("1")
+        .thenReturn("1");
+    when(statisticsQuery.withBreakdowns(any(MongoStatisticsField.class))).thenReturn(statisticsQuery2);
+    when(statisticsQuery.queryForStatistics()).thenReturn(statisticsData);
+    when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
+
+    FilteringResult resultListFilters = statisticsService.queryDataWithFilters(filtersWrapper);
+
+    assertEquals(5, resultListFilters.getResults().getCount());
+    assertEquals("ALL_RECORDS", resultListFilters.getResults().getValue());
+  }
+
+  @Test
+  void queryDataWithFilters_withException() {
+    FiltersWrapper filtersWrapper = new FiltersWrapper();
+    filtersWrapper.setFilters(new StatisticsFilteringRequest(
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsBreakdownValueFilter(0),
+        new StatisticsValueFilter(List.of("1")),
+        new StatisticsRangeFilter("", ""),
+        new StatisticsRangeFilter("", "")
+    ));
+    when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
+
+    Throwable throwable = assertThrows(BreakdownDeclarationFailException.class, () ->
+        statisticsService.queryDataWithFilters(filtersWrapper)
+    );
+
+    assertEquals("There are duplicate or negative breakdowns", throwable.getMessage());
+  }
+}
