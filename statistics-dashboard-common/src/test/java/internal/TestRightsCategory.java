a4d390a56950cc18c4bd3517a6fd077148dc6522
Joana Sousa <joana.sousa@europeana.eu>
2022-04-05 10:48:07 +0200

    MET-4419 Removed code smell

--- statistics-dashboard-common/src/test/java/internal/TestRightsCategory.java
+++ statistics-dashboard-common/src/test/java/internal/TestRightsCategory.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class TestRightsCategory {
+class TestRightsCategory {
 
     @Test
     void testMatchCategoryFromUrlCC0_expectSuccess(){
