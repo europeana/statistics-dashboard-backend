8512c20085ba05b38e9081d4cc4564567246e10d
Joana Sousa <joana.sousa@europeana.eu>
2023-01-31 12:22:03 +0100

    MET-5058 Springbootify the statistics dashboard

---
+++ statistics-dashboard-rest/src/main/java/eu/europeana/statistics/dashboard/rest/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package eu.europeana.statistics.dashboard.rest.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+/**
+ * Config for Swagger documentation
+ */
+@Configuration
+public class SwaggerConfig {
+
+  /**
+   * The open api documentation docket.
+   *
+   * @return the docket configuration
+   */
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Statistics REST API")
+            .description("Statistics Dashboard REST API for Europeana")
+            .version("v1")
+            .license(new License()
+                .name("EUPL Licence v1.2")
+                .url("https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12")));
+  }
+
+}
