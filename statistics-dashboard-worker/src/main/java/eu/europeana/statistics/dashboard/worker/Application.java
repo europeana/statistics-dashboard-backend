9734933e6f6bcaa30601a9b34aeb44511303870d
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2023-04-04 12:06:38 +0200

    MET-5165: Exit spring run on completion

--- statistics-dashboard-worker/src/main/java/eu/europeana/statistics/dashboard/worker/Application.java
+++ statistics-dashboard-worker/src/main/java/eu/europeana/statistics/dashboard/worker/Application.java
@@ -2,6 +2,7 @@ package eu.europeana.statistics.dashboard.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * The Spring boot application entry point.
@@ -9,12 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-  /**
-   * The main spring boot method.
-   *
-   * @param args application arguments
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    /**
+     * The main spring boot method.
+     *
+     * @param args application arguments
+     */
+    public static void main(String[] args) {
+      ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+      run.close();
+    }
 }
