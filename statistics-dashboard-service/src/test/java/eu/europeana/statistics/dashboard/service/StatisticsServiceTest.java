087321cfdc325332eefac7230c0fce214868049b
Simon Tzanakis <Simon.Tzanakis@europeana.eu>
2023-10-09 14:52:31 +0200

    MET-5469: Fix failing test and update mockito

--- statistics-dashboard-service/src/test/java/eu/europeana/statistics/dashboard/service/StatisticsServiceTest.java
+++ statistics-dashboard-service/src/test/java/eu/europeana/statistics/dashboard/service/StatisticsServiceTest.java
@@ -4,8 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 import eu.europeana.statistics.dashboard.common.api.request.FiltersWrapper;
 import eu.europeana.statistics.dashboard.common.api.request.StatisticsBreakdownValueFilter;
@@ -20,13 +24,18 @@ import eu.europeana.statistics.dashboard.service.exception.BreakdownDeclarationF
 import eu.europeana.statistics.dashboard.service.persistence.MongoSDDao;
 import eu.europeana.statistics.dashboard.service.persistence.StatisticsData;
 import eu.europeana.statistics.dashboard.service.persistence.StatisticsQuery;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -43,12 +52,24 @@ class StatisticsServiceTest {
 
   @Mock
   private StatisticsData statisticsData;
-
-  @Mock
   private MongoSDDao mongoSDDao;
-
-  @InjectMocks
   private StatisticsService statisticsService;
+
+  @BeforeEach
+  void initService() {
+    mongoSDDao = mock(MongoSDDao.class);
+    when(statisticsQuery3.queryForStatistics()).thenReturn(statisticsData);
+    when(statisticsQuery2.withValueFilter(any(MongoStatisticsField.class), anyCollection())).thenReturn(statisticsQuery3);
+    when(statisticsQuery.withBreakdowns(any(MongoStatisticsField.class))).thenReturn(statisticsQuery2);
+    when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
+    statisticsService = new StatisticsService(mongoSDDao);
+
+  }
+
+  @AfterEach
+  void closeService() throws Exception {
+    Mockito.reset(mongoSDDao);
+  }
 
   @Test
   void queryGeneralEuropeanaDataWithoutContentTierZero() {
@@ -68,15 +89,15 @@ class StatisticsServiceTest {
         .thenReturn("4");
 
     when(statisticsQuery3.queryForStatistics()).thenReturn(statisticsData);
-    when(statisticsQuery2.withValueFilter(any(MongoStatisticsField.class), any(List.class))).thenReturn(statisticsQuery3);
+    when(statisticsQuery2.withValueFilter(any(MongoStatisticsField.class), anyCollection())).thenReturn(statisticsQuery3);
     when(statisticsQuery.withBreakdowns(any(MongoStatisticsField.class))).thenReturn(statisticsQuery2);
     when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
 
     ResultListFilters resultListFilters = statisticsService.queryGeneralEuropeanaDataWithoutContentTierZero();
 
     assertEquals(7, resultListFilters.getAllBreakdowns().size());
-    assertTrue(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rights", "type").equals(
-        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList())));
+    assertEquals(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rightsCategory", "type"),
+        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList()));
   }
 
   @Test
@@ -103,8 +124,8 @@ class StatisticsServiceTest {
     ResultListFilters resultListFilters = statisticsService.queryGeneralEuropeanaDataIncludingContentTierZero();
 
     assertEquals(7, resultListFilters.getAllBreakdowns().size());
-    assertTrue(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rights", "type").equals(
-        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList())));
+    assertEquals(List.of("contentTier", "country", "dataProvider", "metadataTier", "provider", "rightsCategory", "type"),
+        resultListFilters.getAllBreakdowns().stream().map(b -> b.getBreakdownBy().toString()).collect(Collectors.toList()));
   }
 
   @Test
@@ -133,6 +154,7 @@ class StatisticsServiceTest {
     when(statisticsQuery.queryForStatistics()).thenReturn(statisticsData);
     when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
 
+    statisticsService.refreshRightsUrlsCategoryMapping();
     FilteringResult resultListFilters = statisticsService.queryDataWithFilters(filtersWrapper);
 
     assertEquals(5, resultListFilters.getResults().getCount());
@@ -172,6 +194,7 @@ class StatisticsServiceTest {
     when(statisticsQuery3.queryForStatistics()).thenReturn(statisticsData);
     when(mongoSDDao.createStatisticsQuery()).thenReturn(statisticsQuery);
 
+    statisticsService.refreshRightsUrlsCategoryMapping();
     Set<String> result = statisticsService.getRightsUrlsWithCategory(Set.of(RightsCategory.CC0));
     assertTrue(result.contains("http://exampleurl.org"));
   }
