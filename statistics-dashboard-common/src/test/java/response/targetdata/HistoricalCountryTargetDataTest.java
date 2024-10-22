f4b5083afb8974df5f26e4865e028cc69cf99d10
andyjmaclean <andyjmaclean@gmail.com>
2024-10-22 18:54:18 +0200

    MET-6118 Sonar Fixes

--- statistics-dashboard-common/src/test/java/response/targetdata/HistoricalCountryTargetDataTest.java
+++ statistics-dashboard-common/src/test/java/response/targetdata/HistoricalCountryTargetDataTest.java
@@ -11,13 +11,28 @@ import java.time.LocalDateTime;
 
 class HistoricalCountryTargetDataTest {
 
+    @Test
+    void testNonNullValues_expectSuccess(){
+        HistoricalCountryTargetData historical = new HistoricalCountryTargetData("NL", LocalDateTime.now(), 20L, 30L, 50L);
+        assertEquals(20, historical.getThreeD());
+        assertEquals(30, historical.getHighQuality());
+        assertEquals(50, historical.getTotalNumberRecords());
+    }
 
     @Test
-    void testNullsBecomeZeroes_expectSuccess(){
+    void testNullBecomeZero_expectSuccess(){
         HistoricalCountryTargetData historical = new HistoricalCountryTargetData("NL", LocalDateTime.now(), 0L, 1L, 2L);
         assertNull(historical.getThreeD());
         assertEquals(1, historical.getHighQuality());
         assertEquals(2, historical.getTotalNumberRecords());
+    }
+
+    @Test
+    void testAllNullsBecomeZeroes_expectSuccess(){
+        HistoricalCountryTargetData historical = new HistoricalCountryTargetData("NL", LocalDateTime.now(), 0L, 0L, 0L);
+        assertNull(historical.getThreeD());
+        assertNull(historical.getHighQuality());
+        assertNull(historical.getTotalNumberRecords());
     }
 
 }
